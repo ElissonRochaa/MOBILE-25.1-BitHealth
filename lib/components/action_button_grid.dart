cd83f4b24a3c435481e9cbf88498466838662aaa
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-04-16 21:54:26 -0300

    feat: componente botoes de telas

---
+++ lib/components/action_button_grid.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ActionButtonGrid extends StatelessWidget {
+  const ActionButtonGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final buttons = [
+      _ButtonData("Unidades de Saúde", Icons.location_on),
+      _ButtonData("Médicos", Icons.person),
+      _ButtonData("Plantões", Icons.access_time),
+      _ButtonData("Medicamentos", Icons.medication),
+      _ButtonData("Vacinação", Icons.vaccines),
+      _ButtonData("Serviços", Icons.list),
+      _ButtonData("Notícias", Icons.article),
+      _ButtonData("Notificações", Icons.notifications),
+    ];
+
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+      ),
+      child: GridView.count(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 3.5,
+        children: buttons.map((btn) => _GridButton(data: btn)).toList(),
+      ),
+    );
+  }
+}
+
+class _ButtonData {
+  final String label;
+  final IconData icon;
+
+  _ButtonData(this.label, this.icon);
+}
+
+class _GridButton extends StatelessWidget {
+  final _ButtonData data;
+
+  const _GridButton({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {},
+      icon: Icon(data.icon, color: Colors.blue),
+      label: Text(
+        data.label,
+        style: const TextStyle(color: Colors.blue),
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.blue,
+        elevation: 0,
+        side: const BorderSide(color: Colors.transparent),
+        alignment: Alignment.centerLeft,
+      ),
+    );
+  }
+}
