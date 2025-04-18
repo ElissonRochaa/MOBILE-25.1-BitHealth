45147f0c7f1f0475115516f67d91350282c6ce92
Jeova BL <jeovab115@gmail.com>
2025-04-18 18:09:57 -0300

    feat: implementado botão de navegação dinamico que muda e troca entre paginas

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,11 +1,39 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:bithealth_front_end/pages/home_page.dart';
+import 'package:bithealth_front_end/pages/medicamento_page.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({super.key});
+class BottomNavBar extends StatefulWidget {
+  final int selectedIndex;
+
+  const BottomNavBar({super.key, required this.selectedIndex});
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  void _onItemTapped(int index) {
+    if (index == widget.selectedIndex) return; // evita recriar mesma página
+
+    if (index == 0) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    } else if (index == 3) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MedicamentosPage()),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: widget.selectedIndex,
       selectedItemColor: Colors.blue,
       unselectedItemColor: Colors.grey,
       type: BottomNavigationBarType.fixed,
@@ -16,6 +44,7 @@ class BottomNavBar extends StatelessWidget {
         BottomNavigationBarItem(icon: Icon(Icons.medication), label: "Medicamentos"),
         BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Notificações"),
       ],
+      onTap: _onItemTapped,
     );
   }
 }
