e3e6c6cf794e539d327501f276c953ae9a7ef89e
Jeova BL <jeovab115@gmail.com>
2025-05-17 12:44:02 -0300

    feat: maps

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -7,6 +7,7 @@ class MapaPage extends StatefulWidget {
   const MapaPage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _MapaPageState createState() => _MapaPageState();
 }
 
