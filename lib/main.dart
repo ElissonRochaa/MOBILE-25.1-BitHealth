e8141b36d728f5ef2895a120daeec4c42086285b
Jeova BL <jeovab115@gmail.com>
2025-05-30 09:09:11 -0300

    feat: integrate theme provider for dynamic theming support in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:bithealth_front_end/view/pages/doctor_page.dart';
+import 'package:bithealth_front_end/provider/theme_provider.dart';
 import 'package:bithealth_front_end/view/pages/forgot_password.dart';
+import 'package:bithealth_front_end/theme/app_themes.dart';
 import 'package:bithealth_front_end/view/pages/medical_shifts.dart';
 import 'package:bithealth_front_end/view/pages/register_page.dart';
 import 'package:bithealth_front_end/view/pages/login_page.dart';
@@ -11,11 +13,17 @@ import 'package:bithealth_front_end/view/pages/shift_schedule.dart';
 import 'package:bithealth_front_end/view/pages/vaccination_page.dart';
 import 'package:bithealth_front_end/view/pages/notifications_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'view/pages/home_page.dart';
 import 'view/pages/maps_page.dart';
 
 void main() {
-  runApp(const SaudeCorrentesApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const SaudeCorrentesApp(),
+    ),
+  );
 }
 
 class SaudeCorrentesApp extends StatelessWidget {
@@ -23,29 +31,34 @@ class SaudeCorrentesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Saúde Correntes',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFFEFF6FF),
-      ),
-      home: const HomePage(),
-      routes: {
-        '/home': (context) => const HomePage(),
-        '/medicamentos': (context) => const MedicamentosPage(),
-        '/login': (context) => const LoginScreen(),
-        '/cadastro': (context) => const CadastroScreen(),
-        '/noticias': (context) => const NewsPage(),
-        '/servicos': (context) => const ServicesPage(),
-        '/vacinacao': (context) => const VaccinationPage(),
-        '/medicos': (context) => const DoctorsPage(),
-        '/notificacoes': (context) => const NotificationsPage(),
-        '/mapa': (context) => const MapaPage(),
-        '/plantoes':(context) => const SelecaoPlantaoScreen(),
-        "/escala_plantao":(context) => const EscalaPlantaoScreen(),
-        "/forgot-password": (context) => const ForgotPasswordScreen(),
-        "/resetar-senha": (context) => const ResetPasswordScreen(),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          title: 'Saúde Correntes',
+          debugShowCheckedModeBanner: false,
+          theme: AppThemes.lightTheme,
+          darkTheme: AppThemes.darkTheme,
+          themeMode: themeProvider.isDarkMode 
+              ? ThemeMode.dark 
+              : ThemeMode.light,
+          home: const HomePage(),
+          routes: {
+            '/home': (context) => const HomePage(),
+            '/medicamentos': (context) => const MedicamentosPage(),
+            '/login': (context) => const LoginScreen(),
+            '/cadastro': (context) => const CadastroScreen(),
+            '/noticias': (context) => const NewsPage(),
+            '/servicos': (context) => const ServicesPage(),
+            '/vacinacao': (context) => const VaccinationPage(),
+            '/medicos': (context) => const DoctorsPage(),
+            '/notificacoes': (context) => const NotificationsPage(),
+            '/mapa': (context) => const MapaPage(),
+            '/plantoes': (context) => const SelecaoPlantaoScreen(),
+            "/escala_plantao": (context) => const EscalaPlantaoScreen(),
+            "/forgot-password": (context) => const ForgotPasswordScreen(),
+            "/resetar-senha": (context) => const ResetPasswordScreen(),
+          },
+        );
       },
     );
   }
