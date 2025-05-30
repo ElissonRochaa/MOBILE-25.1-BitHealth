a76ad6de55f61f314618b4d5ab9236d931701da7
Jeova BL <jeovab115@gmail.com>
2025-05-30 09:08:15 -0300

    feat: add ThemeSwitch component for toggling between light and dark themes

---
+++ lib/view/components/theme_switch.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:bithealth_front_end/provider/theme_provider.dart';
+
+class ThemeSwitch extends StatelessWidget {
+  const ThemeSwitch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return Switch(
+          value: themeProvider.isDarkMode,
+          onChanged: (value) {
+            themeProvider.toggleTheme();
+          },
+          activeColor: Colors.blue,
+        );
+      },
+    );
+  }
+}
